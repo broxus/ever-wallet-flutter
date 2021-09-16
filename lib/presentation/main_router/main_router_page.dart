3d137660ce4efbced90c1111d369bf0b14708d9a
Elena Velichko <ev@pmobi.tech>
2021-09-16 09:36:26 +0700

    Remove bottom navigation bar

--- lib/presentation/main_router/main_router_page.dart
+++ lib/presentation/main_router/main_router_page.dart
@@ -1,7 +1,6 @@
 import 'package:back_button_interceptor/back_button_interceptor.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 import '../../logger.dart';
 import '../design/design.dart';
@@ -39,72 +38,6 @@ class MainRouterPageState extends State<MainRouterPage> {
           builder: (context, page, animation) => FadeTransition(
             opacity: Tween<double>(begin: 0.5, end: 1).animate(animation),
             child: page,
-          ),
-          bottomNavigationBuilder: (context, router) => CupertinoTheme(
-            data: const CupertinoThemeData(
-              textTheme: CupertinoTextThemeData(
-                tabLabelTextStyle: TextStyle(
-                  fontFamily: FontFamily.pt,
-                  fontSize: 11,
-                  letterSpacing: 0.2,
-                ),
-              ),
-            ),
-            child: SizedBox(
-              height: context.safeArea.bottom + kBottomBarHeight,
-              child: PlatformNavBar(
-                currentIndex: router.activeIndex,
-                itemChanged: (index) {
-                  if (index == router.activeIndex) {
-                    switch (index) {
-                      case 0:
-                        router.navigate(const WalletRouterRoute());
-                        break;
-                      case 1:
-                        router.navigate(const SettingsRouterRoute());
-                        break;
-                    }
-                  } else {
-                    router.setActiveIndex(index);
-                  }
-                },
-                backgroundColor: CrystalColor.navigationBarBackground,
-                material: (context, target) => MaterialNavBarData(
-                  selectedItemColor: CrystalColor.fontHeaderDark,
-                  unselectedItemColor: const Color(0xFFC4C4C4),
-                ),
-                cupertino: (context, target) => CupertinoTabBarData(
-                  activeColor: CrystalColor.fontHeaderDark,
-                  inactiveColor: const Color(0xFFC4C4C4),
-                ),
-                items: [
-                  BottomNavigationBarItem(
-                    icon: Image.asset(
-                      Assets.images.wallet.path,
-                      width: 24,
-                    ),
-                    activeIcon: Image.asset(
-                      Assets.images.wallet.path,
-                      width: 24,
-                      color: CrystalColor.fontHeaderDark,
-                    ),
-                    label: LocaleKeys.wallet_screen_title.tr(),
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Image.asset(
-                      Assets.images.user.path,
-                      width: 24,
-                    ),
-                    activeIcon: Image.asset(
-                      Assets.images.user.path,
-                      width: 24,
-                      color: CrystalColor.fontHeaderDark,
-                    ),
-                    label: LocaleKeys.settings_screen_title.tr(),
-                  ),
-                ],
-              ),
-            ),
           ),
         ),
       );
