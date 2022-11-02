f4033dc1c84c8953472703596b6f290b63bb571c
AlexA4 <alex.a4.25@yandex.ru>
2022-11-02 17:38:05 +0300

    Added linked keys to account detail

--- lib/application/main/profile/manage_seed/account_detail_screen.dart
+++ lib/application/main/profile/manage_seed/account_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:collection/collection.dart';
 import 'package:ever_wallet/application/bloc/common/account_overall_balance_stream.dart';
 import 'package:ever_wallet/application/common/async_value.dart';
 import 'package:ever_wallet/application/common/async_value_stream_provider.dart';
@@ -6,15 +7,18 @@ import 'package:ever_wallet/application/common/extensions.dart';
 import 'package:ever_wallet/application/common/general/button/menu_dropdown.dart';
 import 'package:ever_wallet/application/common/general/button/push_state_scale_widget.dart';
 import 'package:ever_wallet/application/common/general/default_appbar.dart';
+import 'package:ever_wallet/application/common/general/default_divider.dart';
 import 'package:ever_wallet/application/common/general/default_list_tile.dart';
 import 'package:ever_wallet/application/common/general/flushbar.dart';
 import 'package:ever_wallet/application/main/profile/manage_seed/manage_seed_actions/rename_account_sheet.dart';
 import 'package:ever_wallet/application/main/profile/manage_seed/manage_seed_actions/show_account_delete_sheet.dart';
 import 'package:ever_wallet/application/util/colors.dart';
 import 'package:ever_wallet/application/util/extensions/context_extensions.dart';
+import 'package:ever_wallet/application/util/extensions/iterable_extensions.dart';
 import 'package:ever_wallet/application/util/styles.dart';
 import 'package:ever_wallet/application/util/theme_styles.dart';
 import 'package:ever_wallet/data/repositories/accounts_repository.dart';
+import 'package:ever_wallet/data/repositories/keys_repository.dart';
 import 'package:ever_wallet/data/repositories/token_currencies_repository.dart';
 import 'package:ever_wallet/data/repositories/token_wallets_repository.dart';
 import 'package:ever_wallet/data/repositories/ton_wallets_repository.dart';
@@ -170,22 +174,38 @@ class _AccountDetailScreenState extends State<AccountDetailScreen> {
             ),
           ),
           const SizedBox(height: 16),
-          Expanded(
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                    child: Text(
-                      localization.linked_keys,
-                      style: StylesRes.medium14Caption.copyWith(color: ColorsRes.grey4),
+          if (widget.isExternal)
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                      child: Text(
+                        localization.linked_keys,
+                        style: StylesRes.medium14Caption.copyWith(color: ColorsRes.grey4),
+                      ),
                     ),
-                  ),
-                ],
+                    AsyncValueStreamProvider<List<String>?>(
+                      create: (context) =>
+                          context.read<TonWalletsRepository>().custodiansStream(account.address),
+                      builder: (_, __) {
+                        final publicKeys = context.watch<AsyncValue<List<String>?>>().maybeWhen(
+                              ready: (value) => value,
+                              orElse: () => null,
+                            );
+                        if (publicKeys == null) return _emptyLinkedKeys();
+
+                        return Column(
+                          children: publicKeys.map(_linkedKey).separated(const DefaultDivider()),
+                        );
+                      },
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
         ],
       ),
     );
@@ -240,6 +260,48 @@ class _AccountDetailScreenState extends State<AccountDetailScreen> {
       maxLines: 1,
       minFontSize: 10,
       textAlign: TextAlign.right,
+    );
+  }
+
+  Widget _emptyLinkedKeys() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Text(
+        context.localization.empty_linked_keys,
+        style: StylesRes.basicText,
+      ),
+    );
+  }
+
+  Widget _linkedKey(String publicKey) {
+    final key =
+        context.read<KeysRepository>().keys.firstWhereOrNull((k) => k.publicKey == publicKey);
+    final accounts = context.read<AccountsRepository>().accountsFor(publicKey);
+
+    return EWListTile(
+      leading: Container(
+        height: 32,
+        width: 32,
+        alignment: Alignment.center,
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          color: ColorsRes.darkBlue,
+        ),
+        child: Assets.images.key.svg(width: 18, height: 18),
+      ),
+      titleWidget: Text(
+        key?.name ?? publicKey.ellipsePublicKey(),
+        style: StylesRes.basicText.copyWith(color: ColorsRes.black),
+      ),
+      subtitleWidget: key == null
+          ? null
+          : Text(
+              context.localization.key_name_with_sub_count(
+                publicKey.ellipsePublicKey(),
+                accounts.length,
+              ),
+              style: StylesRes.subtitleStyle.copyWith(color: ColorsRes.grey),
+            ),
     );
   }
 }
