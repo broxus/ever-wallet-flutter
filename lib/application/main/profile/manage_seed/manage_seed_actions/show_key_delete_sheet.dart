f4e5cac43855df8b137101da44dc3d5336913924
AlexA4 <alex.a4.25@yandex.ru>
2022-12-26 19:02:01 +0300

    Split accounts and external in delete sheet

--- lib/application/main/profile/manage_seed/manage_seed_actions/show_key_delete_sheet.dart
+++ lib/application/main/profile/manage_seed/manage_seed_actions/show_key_delete_sheet.dart
@@ -7,6 +7,7 @@ import 'package:ever_wallet/application/util/colors.dart';
 import 'package:ever_wallet/application/util/extensions/context_extensions.dart';
 import 'package:ever_wallet/application/util/styles.dart';
 import 'package:ever_wallet/application/util/theme_styles.dart';
+import 'package:ever_wallet/data/repositories/accounts_repository.dart';
 import 'package:ever_wallet/data/repositories/keys_repository.dart';
 import 'package:ever_wallet/generated/assets.gen.dart';
 import 'package:flutter/material.dart';
@@ -17,7 +18,7 @@ import 'package:nekoton_flutter/nekoton_flutter.dart';
 Future<bool?> showKeyDeleteSheet({
   required BuildContext context,
   required KeyStoreEntry key,
-  required List<AssetsList>? assets,
+  required List<AssetsList> assets,
 }) {
   return showEWBottomSheet<bool>(
     context,
@@ -28,7 +29,7 @@ Future<bool?> showKeyDeleteSheet({
 
 class SeedKeySheet extends StatelessWidget {
   final KeyStoreEntry seedKey;
-  final List<AssetsList>? assets;
+  final List<AssetsList> assets;
 
   const SeedKeySheet({
     super.key,
@@ -41,6 +42,19 @@ class SeedKeySheet extends StatelessWidget {
     final themeStyle = context.themeStyle;
     final localization = context.localization;
 
+    final externalAccountsAddresses =
+        context.read<AccountsRepository>().externalAccounts[seedKey.publicKey] ?? [];
+    final accounts = <AssetsList>[];
+    final externalAccounts = <AssetsList>[];
+
+    assets.forEach((a) {
+      if (externalAccountsAddresses.contains(a.address)) {
+        externalAccounts.add(a);
+      } else {
+        accounts.add(a);
+      }
+    });
+
     return Column(
       mainAxisSize: MainAxisSize.min,
       crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -52,45 +66,51 @@ class SeedKeySheet extends StatelessWidget {
             style: themeStyle.styles.basicStyle.copyWith(color: ColorsRes.text),
           ),
         ),
-        Text(
-          localization.public_key,
-          style: themeStyle.styles.basicStyle.copyWith(color: ColorsRes.grey),
-        ),
-        const SizedBox(height: 8),
-        const DefaultDivider(),
-        EWListTile(
-          leading: Assets.images.seed.svg(height: 32, width: 32),
-          titleWidget: Text(
-            seedKey.name,
-            style: themeStyle.styles.basicStyle.copyWith(color: ColorsRes.text),
-          ),
-          subtitleWidget: Text(
-            seedKey.publicKey.ellipsePublicKey(),
-            style: StylesRes.subtitleStyle.copyWith(color: ColorsRes.grey),
-          ),
-          contentPadding: EdgeInsets.zero,
-        ),
-        Text(
-          localization.accounts_word,
-          style: themeStyle.styles.basicStyle.copyWith(color: ColorsRes.grey),
-        ),
-        const SizedBox(height: 8),
-        const DefaultDivider(),
         Flexible(
           child: SingleChildScrollView(
             child: Column(
               mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: assets
-                      ?.map(
-                        (e) => _accountItem(
-                          e,
-                          themeStyle,
-                          localization,
-                        ),
-                      )
-                      .toList() ??
-                  [],
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  localization.public_key,
+                  style: themeStyle.styles.basicStyle.copyWith(color: ColorsRes.grey),
+                ),
+                const SizedBox(height: 8),
+                const DefaultDivider(),
+                EWListTile(
+                  leading: Assets.images.seed.svg(height: 32, width: 32),
+                  titleWidget: Text(
+                    seedKey.name,
+                    style: themeStyle.styles.basicStyle.copyWith(color: ColorsRes.text),
+                  ),
+                  subtitleWidget: Text(
+                    seedKey.publicKey.ellipsePublicKey(),
+                    style: StylesRes.subtitleStyle.copyWith(color: ColorsRes.grey),
+                  ),
+                  contentPadding: EdgeInsets.zero,
+                ),
+                Text(
+                  localization.my_accounts,
+                  style: themeStyle.styles.basicStyle.copyWith(color: ColorsRes.grey),
+                ),
+                const SizedBox(height: 8),
+                const DefaultDivider(),
+                ...accounts.map(
+                  (e) => _accountItem(e, themeStyle, localization),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: Text(
+                    localization.external_accounts,
+                    style: themeStyle.styles.basicStyle.copyWith(color: ColorsRes.grey),
+                  ),
+                ),
+                const DefaultDivider(),
+                ...externalAccounts.map(
+                  (e) => _accountItem(e, themeStyle, localization),
+                ),
+              ],
             ),
           ),
         ),
