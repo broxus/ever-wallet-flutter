d57ff00f787f6b353b6b20897654029949782605
nesquikm <drapolyuk@gmail.com>
2022-12-28 15:17:42 +0400

    Fix: change svg render lib for (and only!) asset icons https://app.asana.com/0/1202532308671234/1203612689318309/f

--- lib/application/common/widgets/token_asset_icon.dart
+++ lib/application/common/widgets/token_asset_icon.dart
@@ -1,6 +1,6 @@
 import 'package:ever_wallet/application/common/widgets/token_asset_old_label.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:jovial_svg/jovial_svg.dart';
 import 'package:nekoton_flutter/nekoton_flutter.dart';
 
 class TokenAssetIcon extends StatelessWidget {
@@ -19,10 +19,12 @@ class TokenAssetIcon extends StatelessWidget {
         child: Stack(
           children: [
             ClipOval(
-              child: SvgPicture.network(
-                logoURI,
-                width: 36,
-                height: 36,
+              child: ScalableImageWidget.fromSISource(
+                si: ScalableImageSource.fromSvgHttpUrl(
+                  Uri.parse(
+                    logoURI,
+                  ),
+                ),
               ),
             ),
             if (version == TokenWalletVersion.oldTip3v4)
